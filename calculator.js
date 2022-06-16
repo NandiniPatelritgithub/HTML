442722d3192570c5806d82e9c7614182fe8d1fd3
NandiniPatelritgithub <107636549+NandiniPatelritgithub@users.noreply.github.com>
2022-06-16 22:20:41 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function userinput(value){
+    documement.getElementId("userinput").value+=value;
+}function a(){
+    var c=document.getElementById("userinput").value;
+    var d=eval(c);
+    document.getElementById("userinput").value=d;
+}
+
+
